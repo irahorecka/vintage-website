2d97080665dea669879b5d4ff186a0be8a8a079f
Ira Horecka <ira89@icloud.com>
2025-04-02 14:40:19 -0400

    add source code link to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
         <p>
-          Maintained online by Ira Horecka | <a href="#home">Back to Top</a>
+          Maintained online by Ira Horecka | <a href="#home">Back to Top</a> |{' '}
+          <a href="https://github.com/irahorecka/vintage-website">
+            Source Code
+          </a>
         </p>
       </div>
     </footer>
