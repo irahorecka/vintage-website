2190a443856c24adbdb010e376924715bff7a234
Ira Horecka <ira89@icloud.com>
2025-04-06 10:46:24 -0400

    reorder footer links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
           <span>Maintained online by Ira Horecka</span>
           <span>|</span>
           <span>
-            <a href="#home">Back to Top</a>
-          </span>
-          <span>|</span>
-          <span>
             <a href="https://github.com/irahorecka/vintage-website">
               Source Code
             </a>
+          </span>
+          <span>|</span>
+          <span>
+            <a href="#home">Back to Top</a>
           </span>
         </p>
       </div>
